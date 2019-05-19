e3cd68b036491c18d902fe681094bcadd45dc663
Cancain <eriksson.tomas@gmail.com>
2019-05-19 19:45:19 +0200

    1:08:07

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
-
-logger.on("message", data => {
-  console.log(`Called listener`, data);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Homepage</h1>");
+  }
 });
 
-logger.log("Hello World!");
-logger.log("Hi");
-logger.log("Hello!");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
